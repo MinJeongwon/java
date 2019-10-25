36ef8ad82d3bdd6da45633363c2cb5fd97232eeb
user <user@user77-PC>
2019-10-25 10:55:38 +0900

    leap year or common year

---
+++ java/src/method/Method.java
@@ -0,0 +1,24 @@
+package method;
+
+import java.util.Scanner;
+
+public class Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		year();
+	}
+
+	private static void year() {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.print("Is _____ a leap year?    ");
+		System.out.print("Enter the year : ");
+		int year=Integer.parseInt(scn.next());
+		
+		if(year%4==0 && year%100 !=0 || year%400==0)
+			System.out.println("Yes, it is a leap year.");
+		else
+			System.out.println("No, it is a common year.");		
+	}
+}
